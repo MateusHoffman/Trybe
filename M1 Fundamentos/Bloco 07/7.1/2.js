59964cbac47d8f0e104b52308da296fb554286e0
MateusHoffman <mateushoffmandev@gmail.com>
2022-05-10 21:20:53 -0300

    Modify 7.3 and 7.1

---
+++ M1 Fundamentos/Bloco 07/7.1/2.js
@@ -0,0 +1,34 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addNewKey = (obj, key, value) => {
+  obj[key] = value;
+};
+
+console.log(createReport(allLessons, 'Maria Clara'))
+/* {
+  professor: 'Maria Clara',
+  aulas: [ 'Matemática', 'Matemática' ],
+  estudantes: 30
+} */
+
+function counterStudentsMath(lesson) {
+  const numberStudents = 
+}
