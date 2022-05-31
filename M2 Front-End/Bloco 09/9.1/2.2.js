6c9c786e2b9745a479cab90178e52d3b7db0c307
MateusHoffman <mateushoffmandev@gmail.com>
2022-05-31 17:33:06 -0300

    Estudo 9.1

---
+++ M2 Front-End/Bloco 09/9.1/2.2.js
@@ -0,0 +1,15 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return param(userToReturn)
+  // Insira o retorno da função `getUser`
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
---
+++ M2 Front-End/Bloco 09/9.1/4.3.test.js
@@ -0,0 +1,11 @@
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done(error); // Alteramos esta linha
+    }
+  }, 500);
+});
