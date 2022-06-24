051c75dda33a33b8273a4426f12161a4f1e727dc
MateusHoffman <mateushoffmandev@gmail.com>
2022-06-24 14:40:50 -0300

    exercise 11.1 in progress

--- M2 Front-End/Bloco 10/10.2/exercise10.2/src/components/Pokedex.jsx
+++ M2 Front-End/Bloco 10/10.2/exercise10.2/src/components/Pokedex.jsx
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
 import Pokemon from '../components/Pokemon'
+import pokemons from '../data/data'
 
 export class Pokedex extends Component {
+  state = {
+    indexPokemon: 0,
+    arrayPokemons: pokemons,
+  }
+
+  arrayPokemons = (event) => {
+    const {target: { innerText }} = event
+    
+  }
+
+  nextPokemon = () => {
+    const { indexPokemon, arrayPokemons } = this.state
+    if (indexPokemon === arrayPokemons.length - 1) {
+      this.setState({indexPokemon: 0})
+    } else {
+      this.setState({indexPokemon: indexPokemon + 1})
+    }
+  }
+
   render() {
-    // console.log(this.props.pokemons);
     const { pokemons } = this.props
+    const { indexPokemon } = this.state
     return (
       <>
         <h1>Pokedex</h1>
         <div className='pokedex'>
-          {
-            pokemons.map((e) => <Pokemon pokemon={e}/>)
-          }
+          {<Pokemon pokemon={pokemons[indexPokemon]}/>}
         </div>
+        <button
+          className="btn-next-pokemon"
+          type='button'
+          onClick={this.nextPokemon}
+        ><span>Next Pokemon </span></button>
+        {
+          <div className='div-btn-types'>
+            {[...new Set(pokemons.map((e) => e.type))]
+              .map((e) => <button className='btn-types' onClick={this.arrayPokemons}>{e}</button>)}
+          </div>
+        }
       </>
     )
   }
